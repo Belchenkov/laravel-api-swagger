5701c1bf64bea4d9543de866fa0a1461dbd9bb19
Aleksey Belchenkov <u608110@gmail.com>
2021-07-24 10:36:34 +0400

    CRUD for users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,12 +2,76 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\UserResource;
+use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\AnonymousResourceCollection;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
-    public function index()
+    /**
+     * @return AnonymousResourceCollection
+     */
+    public function index(): AnonymousResourceCollection
     {
-        return;
+        $users = User::all();
+        return UserResource::collection($users);
+    }
+
+    /**
+     * @param int $id
+     * @return UserResource
+     */
+    public function show(int $id): UserResource
+    {
+        $user = User::findOrFail($id);
+        return new UserResource($user);
+    }
+
+    /**
+     * @param Request $request
+     * @return Response
+     */
+    public function store(Request $request): Response
+    {
+        $user = User::create([
+            'firstname' => $request->input('firstname'),
+            'lastname' => $request->input('lastname'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password'))
+        ]);
+
+        return response($user, Response::HTTP_CREATED);
+    }
+
+    /**
+     * @param Request $request
+     * @param int $id
+     * @return Response
+     */
+    public function update(Request $request, int $id): Response
+    {
+        $user = User::findOrFail($id);
+
+        $user->update([
+            'firstname' => $request->input('firstname'),
+            'lastname' => $request->input('lastname'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password'))
+        ]);
+
+        return response($user, Response::HTTP_ACCEPTED);
+    }
+
+    /**
+     * @param int $id
+     * @return Response
+     */
+    public function destroy(int $id): Response
+    {
+        User::destroy($id);
+        return response(null, Response::HTTP_NO_CONTENT);
     }
 }

156a2e30e0f630008dc16b06091dcbd48b8c8db5
Aleksey Belchenkov <u608110@gmail.com>
2021-07-25 13:17:54 +0400

    Register endpoint

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('login', 'AuthController@login');
+Route::post('register', 'AuthController@register');
 
 Route::group(['middleware' => 'auth:api'], function () {
     Route::apiResource('users', 'UserController');
